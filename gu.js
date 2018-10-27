7bdcdf0dcc95a456d94d22e2b85240780d079a40
taoyinbbs <taoyinbbs8@gmail.com>
2018-10-27 11:44:01 +0800

    Update gu.js

--- gu.js
+++ gu.js
@@ -1,4 +1,4 @@
-url1="http://tinyurl.com/zp6fpt5";
-url2="http://tao00.net";
-url3="http://t.cn/RVTP2FK";
-url4="http://t.cn/Rcgce0o";
+url1="http://t.cn/RScuE3f";  //a.ta  oy3 6  0.in fo
+url2="http://t.cn/EZ0XLCi";		//8.tao y6.to p
+url3="http://t.cn/RScuuPR";		//sh  ow.tao  yi n666.co m
+url4="http://tinyurl.com/y7npr3gm";		//a.taoy in9.co m
